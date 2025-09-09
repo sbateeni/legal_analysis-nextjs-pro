9d4a2a02f7ec52df4f839d2ccfe3b1bb1bd063d7
sbateeni <121227261+sbateeni@users.noreply.github.com>
2025-09-09 18:12:50 +0300

    Update index.ts

--- frontend/utils/context/index.ts
+++ frontend/utils/context/index.ts
@@ -9,11 +9,17 @@ export * from './types';
 // تصدير المدير الرئيسي
 export { AdvancedContextManager } from './advancedContextManager';
 
-// تصدير مساعدات وأدوات إضافية
-export { createContextInstance, getGlobalContext } from './contextUtils';
-
-// إنشاء مثيل عام للاستخدام
+// استيراد المدير
 import { AdvancedContextManager } from './advancedContextManager';
+
+// دوال مساعدة مباشرة دون استيراد من contextUtils
+export function createContextInstance(): AdvancedContextManager {
+  return AdvancedContextManager.getInstance();
+}
+
+export function getGlobalContext(): AdvancedContextManager {
+  return AdvancedContextManager.getInstance();
+}
 
 // مثيل عام للسياق المتقدم
 export const globalContextManager = AdvancedContextManager.getInstance();
